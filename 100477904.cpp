e5857e3f787c2136e72b43501addb27fa71df2b5
Esmarlyn <joelrh2000@hotmail.com>
2021-10-01 11:43:36 -0400

    Se Trabajo con Funciones

--- 100477904.cpp
+++ 100477904.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include "esconu.h"
+
 using namespace std;
+
 const int max_articulos = 20;
 const int max_datos = 3;
-string devolver(string valor);
+
+void Presentar(string arr[][max_datos], int len);
 void MenuPrincipal();
+
 int main()
 {
     BorraPantalla();
     ifstream Entrada;
     ofstream Salida;
+
     string cod, articulo, marca, precio;
     string guardar[max_articulos][max_datos];
     string respuesta = "";
+
     char opciones;
+
     int cArray = 0, borrar, contadoCorL, contadoCorB;
+
     bool salir = false;
+    
     Entrada.open("BaseDatos.txt");
     Entrada >> cArray;
 
@@ -24,7 +33,7 @@ int main()
     {
         for (int j = 0; j < 3; j++)
         {
-            guardar[i][j] = "~~";
+            guardar[i][j] = "";
         }
     }
 
@@ -44,145 +53,84 @@ int main()
         BorraPantalla();
         switch (opciones)
         {
-        case '1':
-            contadoCorL = 5;
-            BorraPantalla();
-            CoordenadaXY(36, 1);
-            cout << "Listado de articulos en existencia:" << endl;
-            CoordenadaXY(21, 2);
-            cout << "----------------------------------------------------------------------" << endl;
-            CoordenadaXY(21, 3);
-            cout << "| Cod.           Articulo            Marca              Precio       |" << endl;
-            CoordenadaXY(21, 4);
-            cout << "----------------------------------------------------------------------" << endl;
-            for (int i = 0; i < 20; i++)
-            {
-                cout << "    \t\t\t" << i + 1 << ".-";
-                for (int j = 0; j < 3; j++)
-                {
-                    if (j == 0)
-                    {
-                        CoordenadaXY(39, contadoCorL);
-                    }
-                    else if (j == 1)
-                    {
-                        CoordenadaXY(58, contadoCorL);
-                    }
-                    else if (j == 2)
-                    {
-                        CoordenadaXY(77, contadoCorL);
-                    }
-                    cout << guardar[i][j] << " ";
-                }
-                cout << endl;
-                contadoCorL++;
-            }
+            case '1':
+                Presentar(guardar, cArray);
 
-            TeclaPresionada("Precione una Tecla para continuar");
-            BorraPantalla();
-            break;
+                TeclaPresionada("Precione una Tecla para continuar");
+                BorraPantalla();
+                break;
 
-        case '2':
-            BorraPantalla();
-            CoordenadaXY(21, 1);
-            cout << "Registro de Artilulos" << endl;
-            CoordenadaXY(21, 3);
-            cout << "Digite  el Nombre del  Articulo: ";
-            cin >> articulo;
-            CoordenadaXY(21, 5);
-            cout << "Digite  la marca del Articulo: ";
-            cin >> marca;
-            CoordenadaXY(21, 7);
-            cout << "Digite el precio del Articulo: ";
-            cin >> precio;
-            guardar[cArray][0] = articulo;
-            guardar[cArray][1] = marca;
-            guardar[cArray][2] = precio;
-            cArray++;
-            BorraPantalla();
-            break;
+            case '2':
+                BorraPantalla();
+                CoordenadaXY(21, 1);
+                cout << "Registro de Artilulos" << endl;
+                CoordenadaXY(21, 3);
+                cout << "Digite  el Nombre del  Articulo: ";
+                cin >> articulo;
+                CoordenadaXY(21, 5);
+                cout << "Digite  la marca del Articulo: ";
+                cin >> marca;
+                CoordenadaXY(21, 7);
+                cout << "Digite el precio del Articulo: ";
+                cin >> precio;
+                guardar[cArray][0] = articulo;
+                guardar[cArray][1] = marca;
+                guardar[cArray][2] = precio;
+                cArray++;
+                BorraPantalla();
+                break;
 
-        case '3':
-            BorraPantalla();
-            contadoCorB = 5;
-            CoordenadaXY(36, 1);
-            cout << "cual articulo desea eliminar:" << endl;
-            CoordenadaXY(21, 2);
-            cout << "----------------------------------------------------------------------" << endl;
-            CoordenadaXY(21, 3);
-            cout << "| Cod.           Articulo            Marca              Precio       |" << endl;
-            CoordenadaXY(21, 4);
-            cout << "----------------------------------------------------------------------" << endl;
-            for (int i = 0; i < 20; i++)
-            {
-                cout << "    \t\t\t" << i + 1 << ".-";
-                for (int j = 0; j < 3; j++)
-                {
-                    if (j == 0)
-                    {
-                        CoordenadaXY(39, contadoCorB);
-                    }
-                    else if (j == 1)
-                    {
-                        CoordenadaXY(58, contadoCorB);
-                    }
-                    else if (j == 2)
-                    {
-                        CoordenadaXY(77, contadoCorB);
-                    }
-                    cout << guardar[i][j] << " ";
-                }
-                cout << endl;
-                contadoCorB++;
-            }
-            cin >> borrar;
+            case '3':
+                Presentar(guardar, cArray);
+                cout << "Digite el numero de articulo: ";
+                cin >> borrar;
 
-            for (int i = borrar - 1; i < 20; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (guardar[i][j] != "~~")
-                    {
-                        guardar[i][j] = guardar[i + 1][j];
-                    }
-                }
-            }
-            cArray - 1;
-            TeclaPresionada("Exito El articulo se a Borrado Correctamente \n Precione una tecla para continuar");
-            BorraPantalla();
-            break;
-
-        case '4':
-            BorraPantalla();
-            cout << "Deseas Salir S/N?" << endl;
-            cin >> respuesta;
-            if (respuesta == "S" || respuesta == "s")
-            {
-                Salida.open("BaseDatos.txt");
-                Salida << cArray;
-                for (int i = 0; i < 20; i++)
+                for (int i = borrar - 1; i < 20; i++)
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        if (guardar[i][j] != "~~")
+                        if (guardar[i][j] != "")
                         {
-                            Salida << guardar[i][j] << " ";
+                            guardar[i][j] = guardar[i + 1][j];
                         }
                     }
-                    Salida << endl;
                 }
-                Salida.close();
-                salir = true;
-            }
+                cArray--;
+                TeclaPresionada("Exito El articulo se a Borrado Correctamente \n Precione una tecla para continuar");
+                BorraPantalla();
+                break;
 
-            BorraPantalla();
+            case '4':
+                BorraPantalla();
+                cout << "Deseas Salir S/N?" << endl;
+                cin >> opciones;
+                if (opciones == 'S' || opciones == 's')
+                {
+                    Salida.open("BaseDatos.txt");
+                    Salida << cArray;
+                    for (int i = 0; i < 20; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (guardar[i][j] != "~~")
+                            {
+                                Salida << guardar[i][j] << " ";
+                            }
+                        }
+                        Salida << endl;
+                    }
+                    Salida.close();
+                    salir = true;
+                }
 
-            break;
+                BorraPantalla();
 
-        default:
-            BorraPantalla();
-            TeclaPresionada("Esta no es una tecla opcionada Precione una tecla para continuar");
-            break;
+                break;
+
+            default:
+                BorraPantalla();
+                TeclaPresionada("Esta no es una tecla opcionada Precione una tecla para continuar");
+                break;
         }
     } while (salir != true);
     BorraPantalla();
@@ -206,4 +154,43 @@ void MenuPrincipal()
     CoordenadaXY(21, 7);
     cout << "4.-Salir" << endl;
     CoordenadaXY(21, 8);
+}
+
+void Presentar(string arr[][max_datos], int len)
+{
+    BorraPantalla();
+    int contadoCorL = 5;
+    CoordenadaXY(36, 1);
+    cout << "Listado de articulos en existencia:" << endl;
+    CoordenadaXY(21, 2);
+    cout << "----------------------------------------------------------------------" << endl;
+    CoordenadaXY(21, 3);
+    cout << "| Cod.           Articulo            Marca              Precio       |" << endl;
+    CoordenadaXY(21, 4);
+    cout << "----------------------------------------------------------------------" << endl;
+    for (int i = 0; i < len; i++)
+    {
+
+        cout << "    \t\t\t" << i + 1 << ".-";
+        for (int j = 0; j < 3; j++)
+        {
+            if (j == 0)
+            {
+                CoordenadaXY(39, contadoCorL);
+            }
+            else if (j == 1)
+            {
+                CoordenadaXY(58, contadoCorL);
+            }
+            else if (j == 2)
+            {
+                CoordenadaXY(77, contadoCorL);
+            }
+
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+        contadoCorL++;
+    }
 }
